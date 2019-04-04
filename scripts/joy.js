4818bc5ec08fe57294c9ffdfc1967bc701283d45
Daniel O'Mahony <danomahony4@gmail.com>
2019-04-04 10:14:30 +0100

    Add files via upload

---
+++ scripts/joy.js
@@ -0,0 +1,141 @@
+//************************************************//
+      //  DECLARE & INITIALISE VARIABLES //
+//*************************************************//
+
+  var wakeUp = new Audio("audio/feeling.mp3");
+  var shower = new Audio("audio/shower.mp3");
+  var exercise = new Audio("audio/Single_Ladies.mp3");
+  var headphones = new Audio("audio/BB.wav");
+  var sleep = new Audio("audio/Lucky.mp3");
+  var isPlaying = false;
+
+//************************************************//
+      //  TOGGLE PLAY ON WAKE UP BUTTON //
+//*************************************************//
+
+ function togglePlayWakeUp() {
+    if (isPlaying) {
+      wakeUp.pause();
+    }else {
+      wakeUp.play();
+    }
+  }
+
+  wakeUp.onplaying = function() {
+    isPlaying = true;
+  };
+  
+  wakeUp.onpause = function() {
+    isPlaying = false;
+  };
+
+//************************************************//
+      //  TOGGLE PLAY ON SHOWER BUTTON //
+//*************************************************//
+
+  function togglePlayShower() {
+    if (isPlaying) {
+      shower.pause();
+    }else {
+      shower.play();
+    }
+  }
+
+  shower.onplaying = function() {
+    isPlaying = true;
+  };
+  
+  shower.onpause = function() {
+    isPlaying = false;
+  };
+
+  //************************************************//
+      //  TOGGLE PLAY ON EXERCISE BUTTON //
+//*************************************************//
+
+  function togglePlayExercise() {
+    if (isPlaying) {
+      exercise.pause();
+    }else {
+      exercise.play();
+    }
+  }
+
+  exercise.onplaying = function() {
+    isPlaying = true;
+  };
+  
+  exercise.onpause = function() {
+    isPlaying = false;
+  };
+
+
+//************************************************//
+      //  TOGGLE PLAY ON HEADPHONES BUTTON //
+//*************************************************//
+
+ function togglePlayHeadphones() {
+    if (isPlaying) {
+      headphones.pause();
+    }else {
+      headphones.play();
+    }
+  }
+
+  headphones.onplaying = function() {
+    isPlaying = true;
+  };
+  
+  headphones.onpause = function() {
+    isPlaying = false;
+  };
+
+//************************************************//
+      //  TOGGLE PLAY ON SLEEP BUTTON //
+//*************************************************//
+
+  function togglePlaySleep() {
+    if (isPlaying) {
+      sleep.pause();
+    }else {
+      sleep.play();
+    }
+  }
+
+  sleep.onplaying = function() {
+    isPlaying = true;
+  };
+  
+  sleep.onpause = function() {
+    isPlaying = false;
+  };
+
+
+//************************************************//
+      //  STOP SONGS - RESET TO BEGINNING //
+//*************************************************//
+
+function resetWakeUp(){
+  wakeUp.currentTime = 0;
+  wakeUp.pause();
+}
+
+function resetShower(){
+  shower.currentTime = 0;
+  shower.pause();
+}
+
+function resetExercise(){
+  exercise.currentTime = 0;
+  exercise.pause();
+}
+
+function resetHeadphones(){
+  headphones.currentTime = 0;
+  headphones.pause();
+}
+
+function resetSleep(){
+  sleep.currentTime = 0;
+  sleep.pause();
+}
