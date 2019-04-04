978e84c86e49245cc042ff79501dd9be9bdf19ad
Daniel O'Mahony <danomahony4@gmail.com>
2019-04-04 10:30:31 +0100

    raw=1 on audio link

--- scripts/joy.js
+++ scripts/joy.js
@@ -2,10 +2,10 @@
       //  DECLARE & INITIALISE VARIABLES //
 //*************************************************//
 
-  var wakeUp = new Audio("https://www.dropbox.com/s/c1ggyqe9jbvtaq4/Feeling.mp3?dl=0");
-  var shower = new Audio("https://www.dropbox.com/s/icmbahfcljdaq9e/shower.mp3?dl=0");
-  var exercise = new Audio("https://www.dropbox.com/s/99tv5ma2ej4atup/Single_Ladies.mp3?dl=0");
-  var headphones = new Audio("https://www.dropbox.com/s/xuq6199scfa5fxt/BB.wav?dl=0");
+  var wakeUp = new Audio("https://www.dropbox.com/s/c1ggyqe9jbvtaq4/Feeling.mp3?raw=1");
+  var shower = new Audio("https://www.dropbox.com/s/icmbahfcljdaq9e/shower.mp3?raw=1");
+  var exercise = new Audio("https://www.dropbox.com/s/99tv5ma2ej4atup/Single_Ladies.mp3?raw=1");
+  var headphones = new Audio("https://www.dropbox.com/s/xuq6199scfa5fxt/BB.wav?raw=1");
   var sleep = new Audio("https://www.dropbox.com/s/htgtku7a3zmdgjp/Lucky.mp3?raw=1");
   var isPlaying = false;
 
